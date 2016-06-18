67953f7b8d94f04445f08067ca7a509db2d849ee
tonocm <tonocm94@hotmail.com>
2016-06-18 15:41:27 -0700

    better ad experience

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET catalog. */
+router.get('/', function(req, res, next) {
+  res.render('catalog', { title: 'Express' });
+});
+
+/* GET add game. */
+router.get('/', function(req, res, next) {
+  res.render('add', { title: 'Express' });
+});
+
+/* GET about. */
+router.get('/', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+/* GET login. */
+router.get('/', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
 module.exports = router;
